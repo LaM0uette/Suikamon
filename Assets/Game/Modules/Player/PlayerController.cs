25183554b3189398c29a17fc87ef1a4e7891041b
LaM0uette <LaM0uette.dev@gmail.com>
2024-01-07 17:07:55 +0100

    add player controller + player sprite

--- Assets/Game/Modules/Player/PlayerController.cs
+++ Assets/Game/Modules/Player/PlayerController.cs
@@ -1,11 +1,63 @@
 using Game.Modules.Player.Inputs;
+using Sirenix.OdinInspector;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 namespace Game.Modules.Player
 {
     [RequireComponent(typeof(PlayerInputsReader))]
     public class PlayerController : MonoBehaviour
     {
+        #region Statements
+
+        private Camera _mainCamera { get; set; }
+        private PlayerInputsReader _inputReader { get; set; }
         
+        [Space, Title("Player Settings")]
+        [SerializeField] private float _speed = 7f;
+        [SerializeField] private float _maxX = 4.75f;
+
+        private void Awake()
+        {
+            _mainCamera = Camera.main;
+            _inputReader = GetComponent<PlayerInputsReader>();
+        }
+
+        #endregion
+        
+        #region Events
+
+        private void Update()
+        {
+            MouseMove();
+        }
+
+        #endregion
+        
+        #region Functions
+
+        private void MouseMove()
+        {
+            var mousePosition = Mouse.current.position.ReadValue();
+            var worldPosition = _mainCamera.ScreenToWorldPoint(new Vector3(mousePosition.x, mousePosition.y, _mainCamera.nearClipPlane));
+
+            //var offset = _maxX - GameManager.BallOffset;
+            var offset = _maxX;
+            if (worldPosition.x > offset)
+            {
+                worldPosition = new Vector3(offset, worldPosition.y, worldPosition.z);
+            }
+            else if (worldPosition.x < -offset)
+            {
+                worldPosition = new Vector3(-offset, worldPosition.y, worldPosition.z);
+            }
+            
+            var playerTransform = transform;
+            var playerPosition = playerTransform.position;
+            
+            playerTransform.position = new Vector3(worldPosition.x, playerPosition.y, playerPosition.z);
+        }
+
+        #endregion
     }
 }
